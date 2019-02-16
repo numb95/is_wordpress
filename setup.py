17062d0e45984f9a7fb13e21ba9a54bae452e7d3
AmirHossein Goodarzi <amir007ag@gmail.com>
2019-02-16 13:02:38 +0330

    just fix a download_url in setup.py and point it to release archive.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/numb95/is_wordpress",
+    download_url='https://github.com/numb95/is_wordpress/archive/0.0.1.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
