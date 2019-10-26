b168bb248f1ec17705b5edde9e3a377667c94834
Bahador <bahador.eng@gmail.com>
2019-10-26 12:11:53 +0330

    check if url exist

--- is_wordpress/run.py
+++ is_wordpress/run.py
@@ -2,7 +2,12 @@ from . import get_requests, parser
 import sys
 import argparse
 import re
+import requests
+
 def run():
+request = requests.get(args.url)
+if request.status_code == 200:
+    print('Web site exists')
     argparser = argparse.ArgumentParser(description='Check if selected website is based on wordpress or not.It also show the wordpress version.')
     argparser.add_argument("url", help='URL to check the website.')
     args = argparser.parse_args()
@@ -11,3 +16,5 @@ def run():
         return "It's not wordpress or cannot identified"
     if (re.findall('^WordPress.*', parser.parse_data(website))):
         return parser.parse_data(website)
+else:
+    print('Web site does not exist')
